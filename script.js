dbaa38cde52064901660cbf52f0c2f9a9a2fe69d
Adam Elkadri <adam@Adams-MacBook-Pro.local>
2025-02-22 10:02:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => tab.style.display = 'none');
+
+    document.getElementById(tabId).style.display = 'block';
+}
