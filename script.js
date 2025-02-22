c1f71ecd70c490a4af46cb9a1cf024599f60ddbc
Adam Elkadri <adam@Adams-MacBook-Pro.local>
2025-02-22 10:37:30 +0000

    Updated feature XYZ

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function showTab(tabId) {
 
     document.getElementById(tabId).style.display = 'block';
 }
+
+fetch("data/names.json")
+  .then(response => response.json())
+  .then(data => {
+      const list = document.getElementById("names-list");
+      data.forEach(item => {
+          let li = document.createElement("li");
+          li.innerHTML = `<strong>${item.name}</strong> - ${item.meaning} 
+                          <button onclick="playAudio('${item.audio}')">🔊</button>`;
+          list.appendChild(li);
+      });
+  });
+
+function playAudio(audioFile) {
+    let audio = new Audio(audioFile);
+    audio.play();
+}
+
